d860886e374539cf72d4d5d36fe9653017c639d0
xyzuniverse <dev.xyzuniverse@gmail.com>
2024-10-09 04:24:03 +0800

    feat: Add detail for the name of the product depends of their price
    Change-Id: Ifc9fe280ce5c42bcc3fb17f889ef860df29b067d
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- main.js
+++ main.js
@@ -20,7 +20,38 @@ function generateInsertStatements(
 
   while (current_harga <= harga_akhir) {
     const id_barang = `${id_barang_prefix}${String(id).padStart(3, "0")}`;
-    const statement = `(${id}, '${id_barang}', ${id_kategori}, '${nama_barang}', '${merk}', ${harga_beli}, ${current_harga}, '${satuan_barang}', ${stok}, '${tgl_input}', NULL)`;
+    const statement = `(${id}, '${id_barang}', ${id_kategori}, '${nama_barang}${
+      current_harga === 10000 || current_harga === 15000
+        ? " Bening"
+        : current_harga === 20000
+        ? " Hard case"
+        : current_harga === 25000
+        ? " Pro Camera"
+        : current_harga === 30000
+        ? " Pro Camera"
+        : current_harga === 35000
+        ? " Motif bening tipis"
+        : current_harga === 40000
+        ? " Motif"
+        : current_harga === 45000
+        ? " Motif bening tebal"
+        : current_harga === 50000
+        ? " Robot"
+        : current_harga === 55000 && merk.toLowerCase() !== "apple"
+        ? " Hybrid"
+        : current_harga === 55000 &&
+          merk.toLowerCase() === "apple" &&
+          nama_barang.split(" ")[2].replace(/\D/g, "") < 11
+        ? " Hybrid"
+        : (current_harga === 60000 || current_harga === 65000) &&
+          merk.toLowerCase() === "apple" &&
+          nama_barang.split(" ")[2].replace(/\D/g, "") > 10
+        ? " Hybrid"
+        : (current_harga === 60000 || current_harga === 65000) &&
+          merk.toLowerCase() !== "apple"
+        ? " Dompet"
+        : ""
+    }', '${merk}', ${harga_beli}, ${current_harga}, '${satuan_barang}', ${stok}, '${tgl_input}', NULL)`;
     insertStatements.push(statement);
 
     id++;
